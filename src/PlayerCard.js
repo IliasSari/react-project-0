b06ed7254a64ebffff2b28504b6f8ecff5678012
IliasSari <sarigilias@gmail.com>
2026-06-21 11:50:19 +0300

    Added buttons for levels and gave names for buttons on central screengit add .git add .

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,21 +1,34 @@
 import {useState} from "react";
 
-function PlayerCard({name,game,level}){
-    const [likes, setLikes] = useState(0);
+function PlayerCard({name, game, initialLevel}){
+    const [likes, setLikes,] = useState(0);
+    const [level, setLevel] = useState(initialLevel || 1);
 function increaseLikes(){
     setLikes(prev => prev +1);
     }
 function decreaseLikes(){
+    if (likes >0){
     setLikes(prev => prev -1);
     }
+}
+function increaseLevel(){
+    setLevel(prev => prev +1);
+    }
+function decreaseLevel(){
+    if (level>1){
+    setLevel(prev => prev -1);
+    }
+}
     return(
         <div>
             <h2>{name}</h2>
             <p>Game: {game}</p>
             <p>Level: {level}</p>
             <p>Likes: {likes}</p>
-            <button onClick = {increaseLikes}>+</button>
-            <button onClick = {decreaseLikes}>-</button>
+            <button onClick = {increaseLikes}>+ Increase Likes</button>
+            <button onClick = {decreaseLikes}>- Decrease Likes</button>
+            <br></br><button onClick = {increaseLevel}>+ Increase Levels</button>
+            <button onClick = {decreaseLevel}>- Decrease Levels</button>
         </div>
     )
 }
